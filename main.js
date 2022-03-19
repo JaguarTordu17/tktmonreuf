2fec166dac688fd6512097137faf5785364eb9df
JaguarTordu17 <jaguartordu68@gmail.com>
2022-03-19 11:06:36 +0100

    maj

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ function copyToClickBoard(){
         console.log('Something went wrong', err);
     })
 }
+
+document.getElementById('home').onclick = function(){
+    document.location.href = "https://tktmonreuf.tk";
+}
+
+document.getElementById('discord').onclick = function(){
+    window.open("https://discord.gg/cqSj4htav3");
+}
